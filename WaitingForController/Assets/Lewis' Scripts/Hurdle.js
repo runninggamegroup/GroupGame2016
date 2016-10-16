8b82fec7064a8ec42f8d8e254d5013a37f4f9015
A00221443 <a00221443@student.ait.ie>
2016-10-16 23:43:13 +0100

    2 Scenes With Controller-Implemented Movement
    Whatever the title says

---
+++ WaitingForController/Assets/Lewis' Scripts/Hurdle.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+
+}
+
+function OnTriggerEnter(other:Collider)
+{
+	//EditorApplication.isPlaying = false;
+	EditorApplication.isPaused = true;
+}
---
+++ WaitingForController/Assets/Scripts/Jump.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	Jump();
+}
+
+function Jump()
+{
+	if(Input.GetKey(KeyCode.Space))
+	{
+		transform.position.y = 2;
+	}
+	else
+	{
+		transform.position.y = 1;
+	}
+}
